8dc2bde004ef8b3c3989c7b35a4c5b2b0b677c9d
Mallikarjun <rjun912@gmail.com>
2026-01-11 14:28:40 +0530

    added one method second commit

--- src/main/java/demo/HelloGitDemo.java
+++ src/main/java/demo/HelloGitDemo.java
@@ -2,6 +2,9 @@ package demo;
 
 public class HelloGitDemo {
 
+	public static void sayHello() {
+		System.out.println("this is sample method");  
+	}
 	public static void main(String[] args) {
 		System.out.println("This is GIT demo application");
 	}
